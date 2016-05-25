38d59e491e2158d25aaa7ff5fef59a5f82124a44
Chris Palmer <capalmer1013@gmail.com>
2016-05-24 22:20:35 -0400

    writing more "groovy"
    again just kinda writing python/java and it seems to work. although
    nothing in this project actually does anything yet. I'm gonna blame that
    on all the boilerplate code.

--- src/demo/DisplayProvider.groovy
+++ src/demo/DisplayProvider.groovy
@@ -5,10 +5,77 @@ class DisplayProvider {
 
     def text = "Hello from Groovy!"
     def grid = [[0, 0, 0, 0]
-                 [0, 0, 0, 0],
-                 [0, 0, 0, 0],
-                 [0, 0, 0, 0]];
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]];
              
+    Boolean move(String direction){
+        if(direction.equals('l')){
+            for(int i = 0; i < 4; i++){
+                
+            }
+        }
+        else if(direction.equals('r')){
+            for(int i = 0; i < 4; i++){
+                
+            }
+        }
+        else if(direction.equals('u')){
+            for(int i = 0; i < 4; i++){
+                
+            }
+        }
+        else if(direction.equals('d')){
+            for(int i = 0; i < 4; i++){
+                
+            }
+        }
+    }
     
+    Boolean canMove(String direction){
+        Boolean canMove = false;
+        // things needed to be moveable
+        // must either be a space or a collapsable block pair
+        for(int i = 0; i < 4; i++){
+            // Left
+            if(direction.equals('l')){
+                int previousElement = 0
+                Boolean firstNonZero = false
+                Boolean space = false
+                
+                for(int j = 3; j >= 0; i--){
+                    if(grid[i][j] != 0){
+                        firstNonZero = true
+                    }
+                    if(firstNonZero){
+                        if(grid[i][j] == 0){
+                            space = true
+                        }
+                        
+                    }
+                    previousElement = grid[i][j]
+                }
+            }
+            // Right
+            else if(direction.equals('r')){
+                for(int j = 0; j < 4; i++){
+                
+                }
+            }
+            // Up
+            else if(direction.equals('u')){
+                for(int j = 0; j < 4; i++){
+                
+                }
+            }
+            // Down
+            else if(direction.equals('d')){
+                for(int j = 0; j < 4; i++){
+                
+                }
+            }
+        }
+    }
+
 }
 
